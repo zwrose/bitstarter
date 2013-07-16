7e6c9181a7810cb508a4cd9ff5eda4df8e8908d8
Zach Rose <zwrose@gmail.com>
2013-07-16 22:19:33 +0000

    Caught up to end of hw3

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +67,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'URL to process') 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    if('undefined' != typeof program.url) {
+	rest.get(program.url).on('complete', function(result) {
+	    fs.writeFileSync('graderUrl.html',result);
+	});
+	var checkJson = checkHtmlFile('graderUrl.html', program.checks);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
